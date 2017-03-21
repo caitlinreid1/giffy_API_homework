ca2ab14570755a352cc4187b9106008ce9d2bbf3
C. Reid <ctlnrd@gmail.com>
2017-03-20 20:32:51 -0400

    working buttons & input text

--- javascript.js
+++ javascript.js
@@ -1,31 +1,48 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
 var animalArray = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", 
 "ferret", "turtle", "sugarglider", "chinchilla", "hedgehog", "hermit crab", "gerbil", 
 "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
 
-var userinput = $('#animal-input').val();
+
+
+	function renderButtons () {
+
+		$('#buttonsGoHere').empty();
+		//loop through the array to add a new button each time
+		for (var i = 0; i < animalArray.length; i++) {
+			var newButton = $('<button>');
+
+			newButton.addClass("animal")
+			newButton.attr("data-name", animalArray[i])
+			newButton.text(animalArray[i])
+
+			$('#buttonsGoHere').append(newButton);
+		}
+	}
+	
+
+	//when the submit button is clicked...
+	$('#addAnimal').on("click", function(event) {
+
+		event.preventDefault();
+
+		var userInput = $('#animal-input').val().trim();
+
+		animalArray.push(userInput);
+
+		renderButtons();
+	})
+
+renderButtons();	
 
 
 
-for (var i = 0; i < animalArray.length; i++) {
-
-$('#buttonsGoHere').append("<button>" + animalArray[i]+ "</button>")
-
-// console.log(animalArray[i]);
 
 
 
 
-}
-
-
-
-
-
-
-
-}
+// }
 
 
 
