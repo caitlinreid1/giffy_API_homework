6225ce46d4b03e859efc99a03b875a9982150e4a
C. Reid <ctlnrd@gmail.com>
2017-03-20 22:47:06 -0400

    beginning: click: on/off

--- javascript.js
+++ javascript.js
@@ -35,25 +35,28 @@ var animalArray = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird
 
 	})
 
-renderButtons();	
+	renderButtons();	
 
 
 	//when ANY button is clicked...
 	$('button').on("click", function(event){
+		$('#animalGifs').empty();
 
 		var animalType = this.getAttribute("data-name").split(' ').join('+');
 
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q="+animalType+"&api_key=dc6zaTOxFJmzC";
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q="+animalType+"&limit=10&api_key=dc6zaTOxFJmzC";
 
 		$.ajax({
       		url: queryURL,
       		method: 'GET'
     	})
-    	.done(function(response) {
 
+    	.done(function(response) {
      		var results = response.data
 
 	     		for (var i = 0; i < results.length; i++) {
+	    			
+
 	     			var gifDiv = $("<div class='item'>");
 
 	            	var rating = results[i].rating;
@@ -61,34 +64,104 @@ renderButtons();
 	            	var p = $("<p>").text("Rating: " + rating);
 
 	   				var placeholder = $('<img>');
-	   				placeholder.attr("src", results[i]..images.fixed_height.url);
+	   				placeholder.attr("src", results[i].images.fixed_height_still.url);
+	   				placeholder.attr("data-state", "static");
 
 	   				gifDiv.prepend(p);
 	   				gifDiv.prepend(placeholder);
 
 	   				$('#animalGifs').append(gifDiv);
-	   				// $('img').attr("src", results);
-	     		// 	$('img').attr("alt", animalType);
 
-	     		
+	     			$('#animalGifs').append(placeholder); 
 
-	     		$('#animalGifs').append(placeholder);
-	     		}
-    		}); //end the done function
-	})
+	     			// if ($('#animalGifs')).on("click", function(){
+	     			// 	alert("hi");
+	     			// })  				
+
+	     		} //ends for-loop
+    	}); //ends the done function
+	});
+
+
+ 	// deals with the clicking on/off
+	$('#animalGifs').on("click", function() {
+		if ($(this).find("img").attr("data-state") == "static") {
+		
+		function myFunction (){
+
+			console.log("hi");
+
+			var animalType = $('#animalGifs').getAttribute("data-name").split(' ').join('+');
+
+			var queryURL = "http://api.giphy.com/v1/gifs/search?q="+animalType+"&limit=10&api_key=dc6zaTOxFJmzC";
+
+			$.ajax({
+	      		url: queryURL,
+	      		method: 'GET'
+	    	})
+	    	.done(function(response) {
+	     		var movingImage = response.data.images.fixed_height.url;
+
+	     		$(this).find("img").attr("src", response.data.images.fixed_height.url);
+
+	     	});
+
+
+		}
+		
+     	}; //ends done function
+    })
+
+
+
 
 
 
 		
 
+	    // else {
+	    // 	$(this).find("img").attr("src", "staticgif.jpg");
+	    // }
+	
+    //   
+
+
+
+
+		// var animalType = this.getAttribute("data-name").split(' ').join('+');
+
+		// var queryURL = "http://api.giphy.com/v1/gifs/search?q="+animalType+"&limit=10&api_key=dc6zaTOxFJmzC";
+
+		// $.ajax({
+  //     		url: queryURL,
+  //     		method: 'GET'
+  //   	})
+  //   	.done(function(response) {
+
+  //    		var results = response.data
+            	
+  //  			var placeholder = $('<img>');
+
+  //  			placeholder.attr("src", results[i].images.fixed_height_still.url);
+
+  //  				gifDiv.prepend(p);
+  //  				gifDiv.prepend(placeholder);
+
+  //  				$('#animalGifs').append(gifDiv);
+
+  //    			$('#animalGifs').append(placeholder); 
+
+	 //     			// if ($('#animalGifs')).on("click", function(){
+	 //     			// 	alert("hi");
+	 //     			// })  				
+  //   	}); //ends the done function
+   // }
 
 
 
 
 
 
-
-// }
 
 
 
