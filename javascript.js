9e9d43aa3b6f2c9eccd579dd293ac68d9991c51d
C. Reid <ctlnrd@gmail.com>
2017-03-22 23:15:58 -0400

    corrected on/off click functionality

--- javascript.js
+++ javascript.js
@@ -66,8 +66,12 @@ $('button').on("click", function(event){
 
 				var placeholder = $('<img>');
 				placeholder.attr("src", results[i].images.fixed_height_still.url);
+				placeholder.attr("data-still", results[i].images.fixed_height_still.url);
+				placeholder.attr("data-animate", results[i].images.fixed_height.url);
 				placeholder.attr("data-state", "static");
 				placeholder.attr("id", results[i].id)
+				placeholder.attr("class", "imageTag")
+				placeholder.attr("type", results[i].type)
 
 				gifDiv.prepend(p);
 				gifDiv.prepend(placeholder);
@@ -85,6 +89,30 @@ $('button').on("click", function(event){
 
     }) //ends the done function			  	 
 }); // ends button click function
+
+
+$("#animalGifs").on("click", ".imageTag", function() {
+
+	// alert('hi');	
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+
+      // // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // // Then, set the image's data-state to animate
+      // // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+});
+
+
+
+
+
 
 
 
